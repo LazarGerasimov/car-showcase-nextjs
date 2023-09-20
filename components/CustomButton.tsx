0ffc97f189e3065b02406dffaddf682a3b5356b7
Laz <lazgerasimov@gmail.com>
2023-09-20 17:22:32 +0100

    add hero section image; add assets

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -4,16 +4,16 @@ import Image from "next/image";
 
 import { CustomButtonProps } from "@/types";
 
-const CustomButton = ({title, containerStyles, handleClick}: CustomButtonProps) => {
+const CustomButton = ({ title, containerStyles, handleClick }: CustomButtonProps) => {
     return (
         <button
             disabled={false}
             type={'button'}
-            className={`custom-btn`}
-            onClick={() => { }}
+            className={`custom-btn ${containerStyles}`}
+            onClick={handleClick}
         >
             <span className={`flex-1`}>
-                Title
+                {title}
             </span>
         </button>
     )
