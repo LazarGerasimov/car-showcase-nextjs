1df7a14b13b3388d8875b9e710a04c6ea7fa5346
Laz <lazgerasimov@gmail.com>
2023-09-22 19:35:34 +0100

    update CarCard comp; test cards functionality

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,11 +1,28 @@
 "use client";
 
+import { CarProps } from "@/types";
+import { CustomButton } from ".";
 import Image from "next/image";
 
-const CarCard = ({car}: any) => {
-  return (
-    <div>CarCard</div>
-  )
+interface CarCardProps {
+    car: CarProps
+}
+
+const CarCard = ({ car }: CarCardProps) => {
+    const { city_mpg, year, make, model, transmission, drive } = car;
+    return (
+        <div className="car-card group">
+            <div className="car-card__content">
+                <h2 className="car-card__content-title">
+                    {make} {model}
+                </h2>
+            </div>
+
+            <p>
+                <span></span>
+            </p>
+        </div>
+    )
 }
 
 export default CarCard
