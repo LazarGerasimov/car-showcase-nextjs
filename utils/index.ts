8f9df02217720cc28ea35b308d1c7e2d0c12e5e6
Laz <lazgerasimov@gmail.com>
2023-09-25 16:23:42 +0100

    amend and test api call & update params

--- utils/index.ts
+++ utils/index.ts
@@ -1,13 +1,16 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
 // Cars API-Ninja (RapidAPI)
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+
+    const { manufacturer, year, model, limit, fuel } = filters;
+
     const headers = {
         'X-RapidAPI-Key': process.env.CARS_API_KEY as string,
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
 
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera', {
+    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
         headers: headers,
     });
 
